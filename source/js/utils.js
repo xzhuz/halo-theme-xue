c5fa64ab6555e0f12e5876fcab828523e4b1ab7a
Xiao Zai <ms915818993@163.com>
2022-03-30 22:46:45 +0800

    🐛 Fix: 修复看起 pjax 后滚动动画的问题

--- source/js/utils.js
+++ source/js/utils.js
@@ -1,17 +1,24 @@
-function randomStr(len) {
- len = len || 32;
-    　　var $chars = 'ABCDEFGHJKLMNOPQRSTUVWXYZabcdefhijklmnoprstuvwxyz0123456789';     //默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
-    　　var maxPos = $chars.length;
-    　　var pwd = '';
-    　　for (i = 0; i < len; i++) {
-    　　　　pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
-    　　}
-    　　return pwd;
+/**
+ * 随机 id
+ * @param len 长度
+ * @returns {string} id
+ */
+function randomId(len) {
+  len = len || 32;
+  var $chars = 'ABCDEFGHJKLMNOPQRSTUVWXYZabcdefhijklmnoprstuvwxyz0123456789'; 
+  var maxPos = $chars.length;
+  var pwd = '';
+  for (i = 0; i < len; i++) {
+    pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return pwd;
 }
 
-
+/**
+ * 包裹标题
+ */
 function wrapHeader() {
-   $('.md-content').find(':header').each((index, e) => {
-    $(e).attr('id', randomStr(10))
-   });
+  $('.md-content').find(':header').each((index, e) => {
+    $(e).attr('id', randomId(10))
+  });
 }
